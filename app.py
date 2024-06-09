fb927773d22034a1791592be313bc00df203ccb3
sadubey-ind <sadubey097@gmail.com>
2024-06-09 21:17:39 +0530

    Prepare for Heroku deployment

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def upload():
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
