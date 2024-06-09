688b2157836b18b9936d25f971bccfbbdcecfe27
sadubey-ind <sadubey097@gmail.com>
2024-06-09 21:47:13 +0530

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for, flash # type: ignore
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
-app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+# Database setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'suggestions.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Suggestion model
+class Suggestion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    suggestion = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Suggestion {self.name}>'
+
+# Create database
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def index():
@@ -24,6 +45,16 @@ def upload():
         flash(f'File successfully uploaded: {file.filename}')
         return redirect(url_for('index'))
 
+@app.route('/suggest', methods=['POST'])
+def suggest():
+    name = request.form['name']
+    email = request.form['email']
+    suggestion = request.form['suggestion']
+    new_suggestion = Suggestion(name=name, email=email, suggestion=suggestion)
+    db.session.add(new_suggestion)
+    db.session.commit()
+    flash('Thank you for your suggestion!')
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
